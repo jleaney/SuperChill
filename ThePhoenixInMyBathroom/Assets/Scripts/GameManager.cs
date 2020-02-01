4d67e9dc2ca4824e754b9fa52dcce6116c238464
jleaney <jacobleaney@gmail.com>
2020-02-01 17:47:30 +1100

    Added sfx, implemented ending

--- ThePhoenixInMyBathroom/Assets/Scripts/GameManager.cs
+++ ThePhoenixInMyBathroom/Assets/Scripts/GameManager.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public enum GameState
 {
@@ -18,6 +20,19 @@ public class GameManager : MonoBehaviour
     public Transform easelHoldTransform;
 
     public static GameState gameState;
+
+    public static bool inputEnabled = true;
+
+    public GameObject player;
+    public Transform playerOrigin; // transform postion where player starts game and enters for exhibition
+    public AudioClip successSting; // sound played when exhibition starts
+
+    public Image fadePanel;
+    public float fadeSpeed = 2;
+
+    public DialogueManager dialogueManager;
+
+    public ParticleSystem confetti;
 
 	private void Awake()
 	{
@@ -50,5 +65,35 @@ public class GameManager : MonoBehaviour
         int paintingToSpawn = Random.Range(0, paintings.Count);
 
         return Instantiate(paintings[paintingToSpawn], easelHoldTransform.position, easelHoldTransform.rotation);
+    }
+
+    public void TriggerExhibition()
+    {
+        inputEnabled = false;
+        gameState = GameState.Exhibition;
+        fadePanel.gameObject.SetActive(true);
+        fadePanel.DOFade(1, fadeSpeed).OnComplete(ResetPlayerPosition);
+    }
+
+    private void ResetPlayerPosition()
+    {
+        AudioManager.StartExhibition();
+
+        player.transform.position = playerOrigin.position;
+        player.transform.rotation = playerOrigin.rotation;
+
+        // play exhibition dialogue
+        dialogueManager.DisplayExhibitionDialogue();
+    }
+
+    public void StartExhibition()
+    {
+        Debug.Log("Exhibition starting!");
+        confetti.Play(); // confetti raining from the ceiling
+        AudioManager.PlaySFXOneShot(successSting);
+        fadePanel.DOFade(0, fadeSpeed).OnComplete(() =>
+            fadePanel.gameObject.SetActive(false));
+        inputEnabled = true;
+        
     }
 }
