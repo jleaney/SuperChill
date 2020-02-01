e8af20ad965893cb7b63d5cd3a199409461efb47
jleaney <jacobleaney@gmail.com>
2020-02-01 11:17:56 +1100

    Added TMPro and Dialogue System

---
+++ ThePhoenixInMyBathroom/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject dialogueBox;
+    private TextMeshProUGUI text;
+
+    private int currentLine = 0;
+
+    private void Start()
+    {
+        text = dialogueBox.transform.GetComponentInChildren<TextMeshProUGUI>();
+        StartIntroDialogue();
+    }
+
+    public string[] IntroDialogue = new string[5]
+    {
+        "Ah, marvellous, you're here!!",
+        "Just the person we needed to see.",
+        "Our Van Goughs, Dalis, Picassos, and more have all been damaged.",
+        "We need your unmatchable artistic talent to restore them to their original glory before we open tonight!",
+        "I believe in you darling, you can do it!!"
+    };
+
+    public void DisplayDialogue(string[] dialogue)
+    {
+        dialogueBox.SetActive(true);
+        UpdateText(dialogue);
+        StartCoroutine(CheckInput(dialogue));
+    }
+
+    private IEnumerator CheckInput(string[] dialogue)
+    {
+        while(currentLine <= dialogue.Length)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                UpdateText(dialogue);
+            }
+
+            yield return null;
+        }
+
+        currentLine = 0;
+        dialogueBox.SetActive(false); // hides dialogue box
+    }
+
+    private void UpdateText(string[] dialogue)
+    {
+        if (currentLine < dialogue.Length)
+        {
+            text.text = IntroDialogue[currentLine];
+        }
+        
+        currentLine++;
+    }
+
+    public void StartIntroDialogue()
+    {
+        DisplayDialogue(IntroDialogue);
+    }
+}
