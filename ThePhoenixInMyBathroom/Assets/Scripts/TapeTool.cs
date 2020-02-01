0fe25584bcc6f28e5e0ed326d65b87f598d099ef
Howl-Pendragon <jarisrener@gmail.com>
2020-02-01 11:59:21 +1100

    Add WIP Tape tool.

---
+++ ThePhoenixInMyBathroom/Assets/Scripts/TapeTool.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapeTool : Tool
+{
+	public Tape TapePrefab;
+	private Tape _currentTape;
+
+	public override void Use(Vector3 pos)
+	{
+		if (_currentTape == null)
+		{
+			_currentTape = Instantiate(TapePrefab, pos, TapePrefab.transform.rotation);
+			_currentTape.LineRenderer.enabled = false;
+			_currentTape.LineRenderer.SetPosition(0, pos);
+		}
+		else
+		{
+			_currentTape.LineRenderer.SetPosition(1, pos);
+			_currentTape.LineRenderer.enabled = true;
+			_currentTape = null;
+		}
+	}
+
+	public override void Hold(Vector3 pos)
+	{
+	}
+}
