48fb61c710390fe3bdad2e588e3af2a9b243d409
Howl-Pendragon <jarisrener@gmail.com>
2020-02-01 13:10:46 +1100

    Add painting masking.

--- ThePhoenixInMyBathroom/Assets/Scripts/Painting.cs
+++ ThePhoenixInMyBathroom/Assets/Scripts/Painting.cs
@@ -2,16 +2,28 @@
 
 public class Painting : MonoBehaviour
 {
-    public bool LockedToWall { get; set; } // whether painting has been locked to the wall
-    public bool LockedToEasel { get; set; } // whether painting has been locked to the Easel
+	public bool LockedToWall { get; set; } // whether painting has been locked to the wall
+	public bool LockedToEasel { get; set; } // whether painting has been locked to the Easel
 
 	public MeshRenderer MeshRenderer;
 	public Rigidbody Rigidbody;
+	public Texture2D[] Masks;
 
 	public Texture Texture
 	{
 		get => MeshRenderer.material.mainTexture;
 		set => MeshRenderer.material.mainTexture = value;
+	}
+
+	private void Start()
+	{
+		var mask = Masks.GetRandom();
+		var tex = new Texture2D(mask.width, mask.height);
+		Graphics.CopyTexture(mask, tex);
+		tex = tex.ScaleTexture(Texture.width, Texture.height);
+		var newTex = new Texture2D(Texture.width, Texture.height);
+		Graphics.CopyTexture(Texture, newTex);
+		Texture = newTex.AddTextures(tex, new Vector2(0.5f, 0.5f));
 	}
 
 	private void OnMouseOver()
--- ThePhoenixInMyBathroom/Assets/Scripts/Utilities.cs
+++ ThePhoenixInMyBathroom/Assets/Scripts/Utilities.cs
@@ -9,6 +9,21 @@ public static class Utilities
 		return array[rand.Next(0, array.Length)];
 	}
 
+	public static Texture2D ScaleTexture(this Texture2D source, int targetWidth, int targetHeight)
+	{
+		Texture2D result = new Texture2D(targetWidth, targetHeight, source.format, true);
+		Color[] rpixels = result.GetPixels(0);
+		float incX = (1.0f / (float)targetWidth);
+		float incY = (1.0f / (float)targetHeight);
+		for (int px = 0; px < rpixels.Length; px++)
+		{
+			rpixels[px] = source.GetPixelBilinear(incX * ((float)px % targetWidth), incY * ((float)Mathf.Floor(px / targetWidth)));
+		}
+		result.SetPixels(rpixels, 0);
+		result.Apply();
+		return result;
+	}
+
 	public static Texture2D AddTextures(this Texture2D main, Texture2D sticker, Vector2 pos)
 	{
 		int startX = (int)(main.width * pos.x) - (sticker.width / 2);
