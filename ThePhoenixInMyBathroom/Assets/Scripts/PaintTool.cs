68c2e5ee1297cf791a8e4b87d8f1ceefa3ceddd8
Howl-Pendragon <jarisrener@gmail.com>
2020-02-01 18:32:47 +1100

    Fix real bad lag when painting!

--- ThePhoenixInMyBathroom/Assets/Scripts/PaintTool.cs
+++ ThePhoenixInMyBathroom/Assets/Scripts/PaintTool.cs
@@ -4,6 +4,7 @@ public class PaintTool : Tool
 {
 	public Texture2D Brush;
 	public Texture2D[] RotatedBrushes;
+	private Texture2D _original;
 
 	private void Start()
 	{
@@ -29,9 +30,7 @@ public class PaintTool : Tool
 		var painting = GameManager.Instance.Painting;
 		var localPos = painting.transform.InverseTransformPoint(pos);
 		var final = new Vector2(localPos.x + 0.5f, localPos.y + 0.5f);
-		var newTex = new Texture2D(painting.Texture.width, painting.Texture.height);
-		Graphics.CopyTexture(painting.Texture, newTex);
-		painting.Texture = newTex.AddTextures(brush, final);
+		painting.Texture = ((Texture2D)painting.Texture).AddTextures(brush, final);
 	}
 
 	public override void Hold(Vector3 pos, Vector3 normal)
